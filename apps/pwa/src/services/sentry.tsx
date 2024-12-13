e0009501ad11247908af0df0fe595678d99a96e4
Horus Lugo <horusgoul@gmail.com>
2024-12-13 23:30:14 +0100

    Upgrade Sentry, react-router-dom and setup tracing

--- apps/pwa/src/services/sentry.tsx
+++ apps/pwa/src/services/sentry.tsx
@@ -1,6 +1,12 @@
 import { BRANCH, COMMIT_HASH } from "@/constants";
 import * as SentrySDK from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import { useEffect } from "react";
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 
 export function initSentry() {
   if (import.meta.env.DEV) {
@@ -11,7 +17,16 @@ export function initSentry() {
     dsn:
       "https://8c979cf560094d8aac2aa531d72a8a62@o524893.ingest.sentry.io/5638124",
     tunnel: "/api/sentry",
-    integrations: [new Integrations.BrowserTracing()],
+    integrations: [
+      SentrySDK.browserTracingIntegration(),
+      SentrySDK.reactRouterV6BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+    ],
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
     tracesSampleRate: 1.0,
